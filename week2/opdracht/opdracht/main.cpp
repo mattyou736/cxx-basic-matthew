c5e45317d7808b3c34d7c7bf5c4e24b108c34ec8
mattyou736 <36690480+mattyou736@users.noreply.github.com>
2018-10-09 15:36:53 +0200

    update

--- week2/opdracht/opdracht/main.cpp
+++ week2/opdracht/opdracht/main.cpp
@@ -3,10 +3,10 @@
 #include "Student.h"
 
 int main() {
-	Student s = Student(21, "matthew", 2) :Persoon(24, "matthew");
+	Student* s = new Student(21, "matthew", 2) :Persoon(24, "matthew");
 	std::cout << s << std::endl;
 
-	Docent d = Docent("Edwin", "cxx") :Persoon("edwin");
+	Docent* d = new Docent("Edwin", "cxx") :Persoon("edwin");
 	std::cout << d << std::endl;
 
 	return 0;
