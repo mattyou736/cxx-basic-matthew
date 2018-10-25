669313bdbaec86d6cae8aa9c2e2acdca2d8558d6
mattyou736 <36690480+mattyou736@users.noreply.github.com>
2018-10-25 20:49:17 +0200

    good this time

--- week4/week4/main.cpp
+++ week4/week4/main.cpp
@@ -3,54 +3,60 @@
 #include <fstream>
 #include <string>
 
-#define FILE "D:/Desktop/HKU/c++/jabberwocky.txt"
-#define OUTFILE "D:/Desktop/HKU/c++/new.txt"
-
-int main()
+void backwards()
 {
-	char ch;
-	std::ifstream in_stream;
-	in_stream.open(FILE);
+	std::ifstream file("D:/Desktop/HKU/c++/jabberwocky.txt");
+	std::ofstream out("D:/Desktop/HKU/c++/new.txt");
 
-
-	if (!in_stream) {
+	if (file.bad())
+	{
 		std::cout << "problem" << std::endl;
-		exit(1);
 	}
-	
-	std::ofstream out_stream;
-	out_stream.open(OUTFILE);
-	if (!out_stream) {
-		std::cout << "Probleem bij openen output file" << std::endl;
-		exit(1);
+	else
+	{
+		file.seekg(0, std::ios_base::end);//looking for the end of file
+		int file_size = file.tellg();//determining filesize by end of file number
+
+		for (int i = file_size; i > 0; --i)
+		{
+			file.seekg(i - 1, std::ios_base::beg);//reverses order
+			char symbol;
+			file.read(&symbol, 1);
+			out << symbol;//puts it in output file
+		}
 	}
+}
 
+void combine()
+{
+	std::ifstream in[2] =
+	{//get both texts
+		std::ifstream{ "D:/Desktop/HKU/c++/text1.txt" },
+		std::ifstream{ "D:/Desktop/HKU/c++/text2.txt" }
+	};
+	//define output txt name and path
+	std::ofstream out("D:/Desktop/HKU/c++/combine.txt");
+	//while either isnt at end yet
+	while (!in[0].eof() || !in[1].eof())
+	{
+		if (!in[0].eof())
+		{//get first sentences
+			std::string sentence;
+			std::getline(in[0], sentence);
+			out << sentence << std::endl;
+		}
+		//swap between that
+		std::swap(in[0], in[1]);
+	}
+}
 
-	in_stream.get(ch);
-	std::string data;
-
-	in_stream >> std::noskipws;
-
-	//eof = end of file
-	while (!in_stream.eof())
+	void main()
 	{
 		
-		//put() same as
-		in_stream >> ch;
-		//get() same as
-		//std::cout << ch ;
+		backwards();
+		combine();
 
-		
-
-		getline(in_stream, data);
-		
-		std::cout << data << std::endl;	
-		out_stream << data << std::endl;
-		
 	}
 
-	in_stream.close();
-	system("pause");
-	return 0;
-}
+	
 
